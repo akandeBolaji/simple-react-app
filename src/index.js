2caaacb9e7e32bf31926fa84426817676bbd3713
akandeBolaji <akandbolajidanielofficial@gmail.com>
2020-01-29 00:24:05 +0100

    feature: create login and register page

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import { store } from './_helpers';
 import { App } from './App';
 import * as serviceWorker from './serviceWorker';
 
+// setup fake backend
+import { configureFakeBackend } from './_helpers';
+configureFakeBackend();
+
 render(
     <Provider store={store}>
         <App />
