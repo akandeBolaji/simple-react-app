1461f68a4753051b63630f874971d2e28a559a49
akandeBolaji <akandbolajidanielofficial@gmail.com>
2020-02-03 01:36:46 +0100

    Updates

---
+++ precache-manifest.e59ac313be710ccb983aea3de4b20738.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e10078e409ef73567dad79d1c5248f4",
+    "url": "/simple-react-app/index.html"
+  },
+  {
+    "revision": "8263132ba907144126dd",
+    "url": "/simple-react-app/static/js/2.5f130107.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/simple-react-app/static/js/2.5f130107.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13a0a7d4c362401b9f19",
+    "url": "/simple-react-app/static/js/main.31fcc5ef.chunk.js"
+  },
+  {
+    "revision": "43bdbd7584695e892ef3",
+    "url": "/simple-react-app/static/js/runtime-main.cc86d437.js"
+  }
+]);
